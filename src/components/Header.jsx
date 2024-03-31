635f5558de1cab1fb60b03e775bdb9b927bc2ed4
Arjun Gupta <140874081+arjungupta-15@users.noreply.github.com>
2024-03-31 21:58:02 +0530

    commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+   <header className='p-[15px] shadow-xl '>
+    <div className='max-w-[1200px] mx-auto border-orange-500'>
+     hello
+    </div> 
+    <div>
+      
+    </div>
+
+   </header>
+    </>
+  )
+}
+
+export default Header
